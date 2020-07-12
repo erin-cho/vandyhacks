456240f8ba24deaab796af2860d30cce00fd239d
Yerim (Erin) Cho <64560658+erin-cho@users.noreply.github.com>
2020-07-12 05:26:35 -0400

    Add files

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('bored').addEventListener('click', function() {
+    document.getElementById('space').innerHTML = "bored";
+})
+document.getElementById('unproductive').addEventListener('click', function() {
+    document.getElementById('space').innerHTML = "unproductive";
+})
+document.getElementById('lazy').addEventListener('click', function() {
+    document.getElementById('space').innerHTML = "lazy";
+})
